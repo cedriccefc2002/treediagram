6fdd89d91b121d34e7c447abb59367b9fa610e22
曾德雄 <cedriccefc2002@gmail.com>
2018-06-12 13:30:07 +0800

    新增 Server 專案

---
+++ src/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Server
+{
+    class Node : TreeDiagram.NodeDisp_
+    {
+        public override void saveData(string s, Ice.Current current)
+        {
+            Console.WriteLine(s);
+        }
+    }
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                using (Ice.Communicator communicator = Ice.Util.initialize(ref args))
+                {
+                    var adapter =
+                        communicator.createObjectAdapterWithEndpoints("TreeDiagramAdapter", "default -h localhost -p 10000");
+                    adapter.add(new Node(), Ice.Util.stringToIdentity("Node"));
+                    adapter.activate();
+                    communicator.waitForShutdown();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/Slice/Node.ice
@@ -0,0 +1,8 @@
+module TreeDiagram
+{
+    // 節點可儲存資料 (string)
+    interface Node
+    {
+        void saveData(string data);
+    }
+}
