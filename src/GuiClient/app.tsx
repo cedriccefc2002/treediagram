f063783ebf87b15d55973dada8cfd7c394f86c35
曾德雄 <cedriccefc2002@gmail.com>
2018-06-22 12:03:55 +0800

    add bootstrap4 for ui

--- src/GuiClient/app.tsx
+++ src/GuiClient/app.tsx
@@ -1,6 +1,27 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import {Render} from "./lib/Render";
+import { configure } from "log4js";
 
-ReactDOM.render(<Render compiler="TypeScript" framework="React" />, document.getElementById("content"));
+import { Proxy } from "./lib/IceProxy/IceProxy";
+
+import { Render } from "./lib/Render";
+
+if (require.main === module) {
+    (async () => {
+        try {
+            configure({
+                appenders: { console: { type: "console" } },
+                categories: { default: { appenders: ["console"], level: "all" } },
+            });
+            ReactDOM.render(<Render compiler="TypeScript" framework="React" />, document.getElementById("content"));
+            await import("jquery");
+            await import("bootstrap");
+            const proxy = await Proxy.GetProxy();
+            await proxy.init();
+        } catch (error) {
+            alert(error);
+            window.close();
+        }
+    })();
+}
--- src/GuiClient/lib/Render/index.tsx
+++ src/GuiClient/lib/Render/index.tsx
@@ -12,6 +12,38 @@ export interface IRenderState {
 
 export class Render extends React.Component<IRenderProps, IRenderState> {
     public render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <>
+            <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>;
     }
 }
