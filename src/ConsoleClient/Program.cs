b9345575639f37c6663d04cda2ac60c9d4952c11
曾德雄 <cedriccefc2002@gmail.com>
2018-06-27 17:38:04 +0800

    更新Console專案

--- src/ConsoleClient/Program.cs
+++ src/ConsoleClient/Program.cs
@@ -9,19 +9,23 @@ namespace Client
         {
             try
             {
-                using(Ice.Communicator communicator = Ice.Util.initialize(ref args))
+                using (Ice.Communicator communicator = Ice.Util.initialize(ref args))
                 {
-                    var obj = communicator.stringToProxy("Node:default -h localhost -p 10000");
-                    var printer = NodePrxHelper.checkedCast(obj);
-                    if(printer == null)
+                    var obj = communicator.stringToProxy("Server:default -h localhost -p 10000");
+                    var server = ServerPrxHelper.checkedCast(obj);
+                    if (server == null)
                     {
                         throw new ApplicationException("Invalid proxy");
                     }
-                    
-                    printer.saveData("Hello World!");
+
+                    var result = server.listAllTreesAsync().Result;
+                    foreach (var tree in result)
+                    {
+                        Console.WriteLine($"{tree.uuid} {tree.type}");
+                    }
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.Error.WriteLine(e);
                 return 1;
